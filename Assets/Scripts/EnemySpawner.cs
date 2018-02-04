103de88ed836f702f952ae07b1ff6b55f2a343f1
Lara <lara.durrant@gmail.com>
2018-02-04 15:45:07 -0800

    Testing

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+  public GameObject prefab;
+  public Transform leftposition;
+  public Transform rightposition;
+  public Transform topposition;
+  public Transform bleftposition;
+  public Transform brightposition;
+	
+    void Start()
+    {
+		/*
+        for (int i = -2; i < 3; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 1.0F, 0, 0), Quaternion.identity);
+			prefab.transform.parent = transform;
+		}
+		*/
+		
+		foreach ( Transform child in transform) {
+			Instantiate(prefab, child.transform.position, Quaternion.identity);
+			prefab.transform.parent = child;
+			
+		}
+		
+		/*
+		Instantiate(prefab, leftposition.position, Quaternion.identity);
+		Instantiate(prefab, rightposition.position, Quaternion.identity);
+		Instantiate(prefab, topposition.position, Quaternion.identity);
+		Instantiate(prefab, bleftposition.position, Quaternion.identity);
+		Instantiate(prefab, brightposition.position, Quaternion.identity);
+		*/
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Vector3 playerPosition;
+	private Rigidbody2D rb;
+	public float speed = 0.1f;
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		//playerPosition = this.transform.position; 
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		
+		print(playerPosition);
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+            print("Left button is held down");
+			playerPosition = this.transform.position + (Vector3.left * speed * Time.deltaTime);
+			transform.position = new Vector3(Mathf.Clamp(playerPosition.x, -6F, 6.0F), -3f, 0);
+			
+        }
+		
+        if (Input.GetKey(KeyCode.RightArrow))
+		{
+            print("Right button is held down");
+			playerPosition = this.transform.position + (Vector3.right * speed * Time.deltaTime);
+			transform.position = new Vector3(Mathf.Clamp(playerPosition.x, -6F, 6.0F), -3f, 0);
+		}
+	}
+}
